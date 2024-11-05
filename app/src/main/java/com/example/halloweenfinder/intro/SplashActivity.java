1b6f89c6c9a67827f211543b7832ac52389b2de3
mohammad <151404726+Mohammad-Khajavi@users.noreply.github.com>
2024-11-05 11:55:06 -0500

    logo and splash
    added logo and splash for better user experience

--- app/src/main/java/com/example/halloweenfinder/intro/SplashActivity.java
+++ app/src/main/java/com/example/halloweenfinder/intro/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.halloweenfinder.intro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_splash);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
